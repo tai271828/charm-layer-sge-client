84ba460cfc23a4003794bcd0b76d0d20092c0fd1
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2019-01-13 23:39:44 +0800

    Init commit

---
+++ Makefile
@@ -0,0 +1,15 @@
+export CHARM_NAME := sge-client
+export CHARM_BUILDING := ./built-charms
+export CHARM_BUILD_DIR := ${CHARM_BUILDING}/xenial
+
+# Makefile Targets
+deploy-clean-model: build
+	juju destroy-model sge-sandbox -y; juju add-model sge-sandbox; juju switch sge-sandbox
+	juju deploy $(CHARM_BUILD_DIR)/$(CHARM_NAME)
+
+build: clean
+	tox -e build
+
+clean:
+	rm -rf .tox/
+	rm -rf $(CHARM_BUILDING)
---
+++ reactive/sge_client.py
@@ -0,0 +1,34 @@
+from charms.reactive import when, when_not, set_flag
+from charmhelpers.core.hookenv import application_version_set, status_set
+from charmhelpers.fetch import get_upstream_version
+import subprocess as sp
+
+@when_not('sge-client.installed')
+def install_sge_layer():
+    # Do your setup here.
+    #
+    # If your charm has other dependencies before it can install,
+    # add those as @when() clauses above., or as additional @when()
+    # decorated handlers below
+    #
+    # See the following for information about reactive charms:
+    #
+    #  * https://jujucharms.com/docs/devel/developer-getting-started
+    #  * https://github.com/juju-solutions/layer-basic#overview
+    #
+    set_flag('sge-client.installed')
+
+@when('apt.installed.gridengine-client')
+def set_message_hello():
+    # Set the upstream version of hello for juju status.
+    application_version_set(get_upstream_version('gridengine-client'))
+
+    # Run hello and get the message
+    #message = sp.check_output('hello', stderr=sp.STDOUT)
+
+    # Set the active status with the message
+    status_set('active', 'gridengine-client is installed' )
+
+    # Signal that we know the version of hello
+    #set_flag('hello.version.set')
+
