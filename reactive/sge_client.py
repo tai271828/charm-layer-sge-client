70214b2ae72d6254c5d999ad48d088db7e56e48d
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2019-01-21 13:48:20 +0800

    back

--- reactive/sge_client.py
+++ reactive/sge_client.py
@@ -1,20 +1,18 @@
+import subprocess as sp
 from charms.reactive import when, when_not, set_flag, clear_flag
 from charms.reactive.relations import endpoint_from_flag
 from charmhelpers.core import hookenv
 from charmhelpers.core.hookenv import application_version_set, status_set
 from charmhelpers.fetch import get_upstream_version
-import subprocess as sp
 
 @when_not('sge-client.installed')
 @when('apt.installed.gridengine-client')
 def install_sge_layer():
-    # Set the upstream version of hello for juju status.
     application_version_set(get_upstream_version('gridengine-client'))
 
     # Set the active status with the message
-    status_set('active', 'gridengine-client is installed' )
+    status_set('active', 'SGE client is installed' )
 
-    # Signal that we know the version of hello
     set_flag('sge-client.installed')
 
 @when('endpoint.master-config-receiver.new-master')
@@ -23,5 +21,12 @@ def update_mater_config():
     print("hookenv: {}".format(hookenv))
     for master in master_config.masters():
         hookenv.log('master: {}'.format(master['hostname']))
+
+    cmd = ['mkdir', '-p', '/usr/share/charm-sge-cluster/']
+    sp.check_call(cmd)
+    filename = '/usr/share/charm-sge-cluster/master_address'
+    with open(filename, 'w') as fout:
+        fout.write(master['hostname'])
+
     clear_flag('endpoint.master-config-receiver.new-master')
 
