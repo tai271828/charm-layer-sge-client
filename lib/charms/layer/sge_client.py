1d324c2a770a3c1517ead4ca6ff799721ad9363d
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2019-02-08 19:23:09 +0800

    Implement _connect_nfs_server

--- lib/charms/layer/sge_client.py
+++ lib/charms/layer/sge_client.py
@@ -1,16 +1,26 @@
-import os
-import shutil
 import subprocess as sp
 
-__all__ = ['connect_sge_master', 'get_installed_message']
+__all__ = ['connect_sge_master']
+
 
 def connect_sge_master(master_address=None):
-    with open('/etc/profile.d/sge-master', 'w') as fout:
-        fout.write("export MASTER_HOSTNAME={}\n".format(master_address))
+    _connect_sge_master(master_address)
+    _connect_nfs_server(master_address)
 
-    cmd = 'echo ' + master_address + ' | ' + \
+
+def _connect_sge_master(address):
+    cmd = 'echo ' + address + ' | ' + \
           'tee /var/lib/gridengine/default/common/act_qmaster'
     sp.run(cmd, shell=True)
 
     sp.run('service gridengine-exec restart', shell=True)
+
+
+def _connect_nfs_server(address):
+    dir_abs = '/home/ubuntu/mpi_nfs_mnt'
+    cmd = 'mkdir ' + dir_abs
+    sp.run(cmd, shell=True)
+
+    cmd = 'mount -t nfs ' + address + ':' + dir_abs + ' ' + dir_abs
+    sp.run(cmd, shell=True)
 
